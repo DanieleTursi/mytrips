2d728cb8deec74a26ac3c342376c3857753e9b72
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-10-10 20:03:12 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import Navbar from './components/Navbar/Navbar';
 import './App.css';
 
-function App() {
+
+
+class App extends Component {
+  componentDidMount(){
+    document.body.style.backgroundColor = "red"
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
     </div>
   );
+}
 }
 
 export default App;
