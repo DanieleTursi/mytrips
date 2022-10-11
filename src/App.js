27c24cb441e6cbbaca3539f4e1aa5fde755fc50f
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-10-11 19:30:23 +0100

    countries card component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
-import React, {Component} from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import Navbar from './components/Navbar/Navbar';
+import Card from './components/Card/Card';
 import './App.css';
+import {Helmet} from 'react-helmet';
 
 
 
-class App extends Component {
-  componentDidMount(){
-    document.body.style.backgroundColor = "red"
-  }
-
-  render(){
-  return (
-    <div className="App">
-      <Navbar/>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state ={
+        countries:[ITALY, FRANCE, SPAIN, POLAND],
+        colors:[{ITALY:'linear-gradient(to right, green, white, red)'}],[{FRANCE:'linear-gradient(to right, blue, white, red)'}]
+    }
 }
+  render(){ 
+    return(
+    <div className="App">
+        <style jsx global>{`
+      body {
+        margin: 0px;
+        padding: 0px;
+        background:#C6D3E6;
+      }
+    `}</style>
+      <Navbar/>
+      <Card/>
+    </div>
+    )
+  };
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,11 @@ const Navbar = ()=>{
     return(
       <div className="container">
         <h1>MYTRIPS</h1>
+        <a>AFRICA</a>
+        <a>AMERICA</a>
+        <a>ASIA</a>
+        <a>EUROPE</a>
+        <a>OCEANIA</a>
       </div>
     )
 };
