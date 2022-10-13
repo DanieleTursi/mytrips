d42597a121e789963559f64e63e17e00a8f103ba
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-10-13 13:15:03 +0100

    countries and color flags implemented

--- src/components/Card/countries.js
+++ src/components/Card/countries.js
@@ -1,12 +1,198 @@
 export const europe = [
     {
         id: 1,
-        name: 'ITALY',
-        bg:{background:'linear-gradient(to right, green, white, red)'},
+        name: 'AUSTRIA',
+        bg:{background:'linear-gradient(to bottom, #EF3340, white, #EF3340)'},
     },
     {
         id: 2,
+        name: 'BELGIUM',
+        bg:{background:'linear-gradient(to right, black, #FFCD00, #C8102E)'},
+        },
+
+    {
+        id: 3,
+        name: 'BOSNIA AND HERZEGOVINA',
+        bg:{background:'linear-gradient(to right, #002F6C, white, #FFCD00)'},
+    },
+    {
+        id: 4,
+        name: 'BULGARIA',
+        bg:{background:'linear-gradient(to bottom, white, #00966E, #D62612)'},
+        },
+            
+    {
+        id: 5,
+        name: 'CROATIA',
+        bg:{background:'linear-gradient(to right, #E62319, #0F378C, white)'},
+    },
+    {
+        id: 6,
+        name: 'CYPRUS',
+        bg:{background:'linear-gradient(to right, #D57800,white, #4E5B31)'},
+        },
+    {
+        id: 7,
+        name: 'CZECH REPUBLIC',
+        bg:{background:'linear-gradient(to right, #11457E, white, #D7141A)'},
+    },
+    {
+        id: 8,
+        name: 'DENMARK',
+        bg:{background:'linear-gradient(to right, #C8102E, white)'},
+        },
+
+    {
+        id: 9,
+        name: 'ENGLAND',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+    },
+    {
+        id: 10,
+        name: 'FINLAND',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+        },
+            
+    {
+        id: 11,
+        name: 'FRANCE',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+    },
+    {
+        id: 12,
+        name: 'GERMANY',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+        },
+    {
+        id: 13,
+        name: 'GIBRALTAR',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+    },
+    {
+        id: 14,
+        name: 'GREECE',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+        },
+
+    {
+        id: 15,
         name: 'HUNGARY',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 16,
+        name: 'ITALY',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+        },
+            
+    {
+        id: 17,
+        name: 'LATVIA',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+    },
+    {
+        id: 18,
+        name: 'LITHUANIA',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 19,
+        name: 'MALTA',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 20,
+        name: 'MONACO',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+        },
+            
+    {
+        id: 21,
+        name: 'MONTENEGRO',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+    },
+    {
+        id: 22,
+        name: 'NORWAY',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 23,
+        name: 'POLAND',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+    },
+    {
+        id: 24,
+        name: 'PORTUGAL',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+        },
+
+    {
+        id: 25,
+        name: 'ROMANIA',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 26,
+        name: 'RUSSIA',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+        },
+            
+    {
+        id: 27,
+        name: 'SAN MARINO',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+    },
+    {
+        id: 28,
+        name: 'SCOTLAND',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 29,
+        name: 'SLOVAKIA',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 30,
+        name: 'SLOVENIA',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+        },
+            
+    {
+        id: 31,
+        name: 'SPAIN',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+    },
+    {
+        id: 32,
+        name: 'SWEDEN',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 33,
+        name: 'SWITZERLAND',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 34,
+        name: 'THE NETHERLANDS',
+        bg:{background:'linear-gradient(to bottom, red, white, green)'},
+    },
+    {
+        id: 35,
+        name: 'THE VATICAN',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+        },
+            
+    {
+        id: 36,
+        name: 'UKRAINE',
+        bg:{background:'linear-gradient(to right, green, white, red)'},
+    },
+    {
+        id: 37,
+        name: 'WALES',
         bg:{background:'linear-gradient(to bottom, red, white, green)'},
         },
 
