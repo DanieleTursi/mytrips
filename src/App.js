90a00d03ff26439ad5f5998060c843aa94198aaa
DanieleTursi <99504866+DanieleTursi@users.noreply.github.com>
2022-10-16 17:15:26 +0100

    implementing react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,33 +3,63 @@ import Navbar from './components/Navbar/Navbar';
 import Card from './components/Card/Card';
 import './App.css';
 import {Helmet} from 'react-helmet';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 
 
 class App extends Component{
-  constructor(){
-    super()
-    this.state ={
-        europe:['AUSTRIA','BELGIUM','BOSNIA & HERZAGOVINA', 'BULGARIA'],
-        searchfield:''
-    }
-}
+  // constructor(){
+  //   super()
+  //   this.state ={
+  //       europe:['AUSTRIA','BELGIUM','BOSNIA & HERZAGOVINA', 'BULGARIA'],
+  //       searchfield:''
+  //   }
+  // }
   render(){ 
     const {europe}= this.state;
     return(
-    <div className="App">
-        <style jsx global>{`
-      body {
-        margin: 0px;
-        padding: 0px;
-        background:#FFFFFF;
-      }
-    `}</style>
+      <Router>
+         <div className="App">
+             <style jsx global>{`
+                 body {
+                 margin: 0px;
+                 padding: 0px;
+                 background:#FFFFFF;
+                  }
+              `}</style>
       <Navbar/>
-      <Card europe={europe}/>
-    </div>
+      <Routes>
+
+      <Route path="/" element={<Card europe={europe}/>}/>
+        
+
+      {/* <Route path={"/africa"}>
+        <Africa />
+      </Route>
+
+      <Route path={"/america"}>
+        <America />
+      </Route>
+
+      <Route path={"/asia"}>
+        <Asia />
+      </Route>
+
+      <Route path={"/europe"}>
+        <Europe />
+      </Route>
+
+      <Route path={"/oceania"}>
+        <Oceania />
+      </Route> */}
+
+      </Routes>
+      </div>
+    </Router>
     )
   };
+
+  
 }
 
 export default App;
